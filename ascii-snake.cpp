8adf7630a3e30887a146c7c97e28daee32b8f5ea
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-09 00:00:29 -0400

    Finised base game

--- ascii-snake.cpp
+++ ascii-snake.cpp
@@ -14,11 +14,14 @@ std::list<int> snake;
 const int field_width = 120;
 const int field_height = 24;
 wchar_t field[field_height*field_width];
+int nCurrentX = 0;
+int nCurrentY = 0;
 enum Direction { NORTH, WEST, SOUTH, EAST };
 
 void InitField();
 void MoveSnake(Direction dir);
-
+void GrowSnake(Direction dir);
+bool InBounds(int curX, int curY);
 int main()
 {
 	// Create Screen Buffer
@@ -28,12 +31,15 @@ int main()
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
+	swprintf_s(&screen[2 * nScreenWidth + nScreenWidth - 28], 28, L"--A game by: A. D. Ozkazanc");
+
+	swprintf_s(&screen[(nScreenHeight - 2) * nScreenWidth + nScreenWidth - 19], 19, L"Press 'q' to quit.");
+
 	InitField();
-	
-	
-	//Init snake at the middle of the board
-	int nCurrentX = field_width / 2;
-	int nCurrentY = field_height / 2;
+		
+	//Initialize snake at the middle of the board
+	nCurrentX = field_width / 2;
+	nCurrentY = field_height / 2;
 	int nStartIndex = nCurrentY * field_width + nCurrentX;
 	snake.push_back(nStartIndex);
 	snake.push_back(nStartIndex + 1);
@@ -41,17 +47,17 @@ int main()
 	snake.push_back(nStartIndex + 3);
 
 	int bKey[5];
+	int score = 0;
 	bool bGameOver = false;
-	bool bForceMove;
-	int nPlaySpeed = 2;
-	int nTickCount = 0;
+
+	int nFoodIndex = 0;
+	int nFoodCount = 0;
+	bool bNewFood = true;
 	Direction dir = WEST;
 	
 	while (!bGameOver) {
 		//Timing
-		std::this_thread::sleep_for(std::chrono::milliseconds(50)); // Small Step = 1 Game Tick
-		nTickCount++;
-		bForceMove = nTickCount == nPlaySpeed;
+		std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Small Step = 1 Game Tick
 		
 		//Input
 		for (int k = 0; k < 5; k++)								 // R   L   D   U Q
@@ -59,40 +65,75 @@ int main()
 
 		//Game Logic
 		//Press Q to quit
-		if (bKey[4]) break;
+		if (bKey[4]) bGameOver = true;
 
 		// Handle player movement
-		dir = bKey[0] ? EAST : dir;
-		dir = bKey[1] ? WEST : dir;
-		dir = bKey[2] ? SOUTH : dir;
-		dir = bKey[3] ? NORTH : dir;
+		// Check arrow key presses and make sure that the snake cannot turn into itself
+		// only register 1 key press at a time
+		if		(bKey[0] && dir != WEST)	dir = EAST;
+		else if (bKey[1] && dir != EAST)	dir = WEST;
+		else if (bKey[2] && dir != NORTH)	dir = SOUTH;
+		else if (bKey[3] && dir != SOUTH)	dir = NORTH;
+	
+		//Spawn food
+		if (bNewFood) {
+			nFoodCount++;
+			// do not want to spawn food on the walls
+			int nFoodX = (rand() % (field_width - 2)) + 1;
+			int nFoodY = (rand() % (field_height - 2)) + 1;
 
-		//TODO: Make sure that the snake cannot turn into itself
-		
-		if (bForceMove) {
-			nTickCount = 0;
-			MoveSnake(dir);
+			nFoodIndex = nFoodY * field_width + nFoodX;
+			bNewFood = false;
+			
+			//TODO: Spawn a higher valued timed food  in every 5 foods
 		}
+
+		MoveSnake(dir);
+			
+		if (!InBounds(nCurrentX,nCurrentY)) bGameOver = true;
+		
+		//Snake eats the food
+		if (snake.front() == nFoodIndex) {
+			score += 10;
+			GrowSnake(dir);
+
+			bNewFood = true;
+			swprintf_s(&screen[nFoodIndex + 4 * nScreenWidth], 2, L" ");
+		}
+		
+		//Does the snake interrsect with itself?
+		for (auto it = snake.begin(); it != snake.end(); it++)
+			if (it != snake.begin() && *it == snake.front())
+				bGameOver = true;
+		
 
 		//Display
 		//Draw the field
-	
 		for (int i = 0; i < field_height; i++) {
 			for (int j = 0; j < field_width; j++)
 				screen[(4 + i) * nScreenWidth + j] = field[i * field_width + j];
 		}
 	
-		//Draw the snake
+		//Draw the food
+		swprintf_s(&screen[nFoodIndex + 4 * nScreenWidth], 2, L"$");
 
+		//Draw the snake
 		for (auto it = snake.begin(); it != snake.end(); it++) {
 			if(it == snake.begin()) screen[*it + 4 * nScreenWidth] = L'@';
 			else screen[*it + 4 * nScreenWidth] = L'*';
 		}
+		//Draw the score
+		swprintf_s(&screen[2 * nScreenWidth], 16, L"SCORE: %8d", score);
+		
+		//Drwa the snake length
+		swprintf_s(&screen[2 * nScreenWidth + 20], 23, L"Snake Length: %8d", snake.size());
+
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
 	
 	}
 	CloseHandle(hConsole);
-	//std::cout << "Game Over!! Score:" << score << std::endl;
+	std::cout << "Game Over, Score:" << score << std::endl;
+	std::cout << "Thanks for playing!" << std::endl;
 	std::cin.get();
 	return 0;
 }
@@ -108,17 +149,42 @@ void MoveSnake(Direction dir) {
 	switch (dir) {
 	case NORTH:
 		nNextIndex -= field_width;
+		nCurrentY--;
 		break;
 	case WEST:
 		nNextIndex -= 1;
+		nCurrentX--;
 		break;
 	case SOUTH:
 		nNextIndex += field_width;
+		nCurrentY++;
 		break;
 	case EAST:
 		nNextIndex += 1;
+		nCurrentX++;
 	}
 	snake.push_front(nNextIndex);
 	snake.pop_back();
 }
+bool InBounds(int curX, int curY){
+	return curX > 0 && curY > 0 && curX < field_width - 1 && curY < field_height - 1;
+}
+void GrowSnake(Direction dir) {
+	int nNewTail = snake.back();
+	switch (dir) {
+	case NORTH:
+		nNewTail += field_width;
+		break;
+	case WEST:
+		nNewTail += 1;		
+		break;
+	case SOUTH:
+		nNewTail -= field_width;		
+		break;
+	case EAST:
+		nNewTail -= 1;
+	}
+	snake.push_back(nNewTail);
+}
+
 
